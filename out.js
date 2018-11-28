d4373aef49159897caaa1cdbab80ac56ed4a153d
Beniamin Sawczak <beniaminsawczak21@gmail.com>
2018-11-28 18:47:12 +0100

    crypto

---
+++ out.js
@@ -0,0 +1,38 @@
+let p = 13;
+let q = 11;
+
+function cryptoModule(a,b){
+  result = a*b
+  console.log(result)
+  return result
+}
+function euler(a,b){
+  let result =(a-1)*(b-1)
+  console.log(result + ' euler')
+  return result
+}
+
+
+let a  = euler(p,q)
+let b = cryptoModule(p,q)
+
+function eNumber(arg1,arg2){
+  for( let i=0; i < arg2; i++){
+    if(euklides(arg1, i) === 1){
+      console.log(i)
+    }
+  }
+}
+
+eNumber(a,b)
+
+function euklides(a,b){
+  while(a != b){
+    if(a < b){
+      b = b - a
+    }else{
+      a = a -b
+    }
+  }
+  console.log(a)
+}
